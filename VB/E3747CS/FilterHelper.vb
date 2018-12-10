4d30d8f2679067c3f72f79a2147a5ca96b4c4d30
DevExpressExampleBot <fakeEmail>
2018-12-10 14:11:48 +0300

    Source auto update [skip ci]

---
+++ VB/E3747CS/FilterHelper.vb
@@ -0,0 +1,55 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Text
+Imports DevExpress.XtraGrid.Views.Grid
+Imports DevExpress.Data
+Imports System.Data
+Imports System.ComponentModel
+Imports DevExpress.Data.Filtering.Helpers
+Imports System.Collections
+
+Namespace FilterMasterDetailGrid
+    Public Class FilterHelper
+        Private _parentView As GridView
+        Private _childView As GridView
+
+        Public Sub New(ByVal parentView As GridView, ByVal childView As GridView)
+            _parentView = parentView
+            _childView = childView
+            AddHandler _parentView.CustomRowFilter, AddressOf _view_CustomRowFilter
+        End Sub
+
+        Private Sub _view_CustomRowFilter(ByVal sender As Object, ByVal e As DevExpress.XtraGrid.Views.Base.RowFilterEventArgs)
+            Dim view As GridView = (TryCast(sender, GridView))
+            Dim controller As BaseGridController = view.DataController
+            Dim pdc As PropertyDescriptorCollection = Nothing
+                Dim source As IList = DirectCast(view.DataSource, IList)
+            If TypeOf view.DataSource Is ITypedList Then
+                pdc = (TryCast(view.DataSource, ITypedList)).GetItemProperties(Nothing)
+            Else
+                pdc = TypeDescriptor.GetProperties(DirectCast(source, Object).GetType().GetProperty("Item").PropertyType)
+            End If
+            Dim ev As New ExpressionEvaluator(pdc, controller.FilterCriteria)
+            e.Visible = Not IsEmptyDetail(e.ListSourceRow, controller) AndAlso ev.Fit(source(e.ListSourceRow))
+            e.Handled = True
+        End Sub
+
+
+        Private Function IsEmptyDetail(ByVal listSourceRow As Integer, ByVal controller As BaseGridController) As Boolean
+            Dim detail As IList = DirectCast(controller.GetDetailList(listSourceRow, 0), IList)
+            Dim pdc As PropertyDescriptorCollection = Nothing
+            If TypeOf detail Is ITypedList Then
+                pdc = (TryCast(detail, ITypedList)).GetItemProperties(Nothing)
+            Else
+                pdc = TypeDescriptor.GetProperties(DirectCast(detail, Object).GetType().GetProperty("Item").PropertyType)
+            End If
+            Dim detailEvaluator As New ExpressionEvaluator(pdc, _childView.DataController.FilterCriteria)
+            For Each o As Object In detail
+                If detailEvaluator.Fit(o) Then
+                    Return False
+                End If
+            Next o
+            Return True
+        End Function
+    End Class
+End Namespace
---
+++ VB/E3747CS/Form1.designer.vb
@@ -0,0 +1,140 @@
+﻿Namespace FilterMasterDetailGrid
+    Partial Public Class Form1
+        ''' <summary>
+        ''' Required designer variable.
+        ''' </summary>
+        Private components As System.ComponentModel.IContainer = Nothing
+
+        ''' <summary>
+        ''' Clean up any resources being used.
+        ''' </summary>
+        ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+            If disposing AndAlso (components IsNot Nothing) Then
+                components.Dispose()
+            End If
+            MyBase.Dispose(disposing)
+        End Sub
+
+        #Region "Windows Form Designer generated code"
+
+        ''' <summary>
+        ''' Required method for Designer support - do not modify
+        ''' the contents of this method with the code editor.
+        ''' </summary>
+        Private Sub InitializeComponent()
+            Dim gridLevelNode1 As New DevExpress.XtraGrid.GridLevelNode()
+            Me.gridView2 = New DevExpress.XtraGrid.Views.Grid.GridView()
+            Me.gridControl1 = New DevExpress.XtraGrid.GridControl()
+            Me.gridView1 = New DevExpress.XtraGrid.Views.Grid.GridView()
+            Me.simpleButton1 = New DevExpress.XtraEditors.SimpleButton()
+            Me.panelControl1 = New DevExpress.XtraEditors.PanelControl()
+            Me.simpleButton4 = New DevExpress.XtraEditors.SimpleButton()
+            Me.simpleButton3 = New DevExpress.XtraEditors.SimpleButton()
+            Me.simpleButton2 = New DevExpress.XtraEditors.SimpleButton()
+            CType(Me.gridView2, System.ComponentModel.ISupportInitialize).BeginInit()
+            CType(Me.gridControl1, System.ComponentModel.ISupportInitialize).BeginInit()
+            CType(Me.gridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+            CType(Me.panelControl1, System.ComponentModel.ISupportInitialize).BeginInit()
+            Me.panelControl1.SuspendLayout()
+            Me.SuspendLayout()
+            ' 
+            ' gridView2
+            ' 
+            Me.gridView2.GridControl = Me.gridControl1
+            Me.gridView2.Name = "gridView2"
+            ' 
+            ' gridControl1
+            ' 
+            Me.gridControl1.Dock = System.Windows.Forms.DockStyle.Fill
+            gridLevelNode1.LevelTemplate = Me.gridView2
+            gridLevelNode1.RelationName = "Detail"
+            Me.gridControl1.LevelTree.Nodes.AddRange(New DevExpress.XtraGrid.GridLevelNode() { gridLevelNode1})
+            Me.gridControl1.Location = New System.Drawing.Point(0, 36)
+            Me.gridControl1.MainView = Me.gridView1
+            Me.gridControl1.Name = "gridControl1"
+            Me.gridControl1.ShowOnlyPredefinedDetails = True
+            Me.gridControl1.Size = New System.Drawing.Size(790, 445)
+            Me.gridControl1.TabIndex = 0
+            Me.gridControl1.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() { Me.gridView1, Me.gridView2})
+            ' 
+            ' gridView1
+            ' 
+            Me.gridView1.GridControl = Me.gridControl1
+            Me.gridView1.Name = "gridView1"
+            ' 
+            ' simpleButton1
+            ' 
+            Me.simpleButton1.Location = New System.Drawing.Point(12, 5)
+            Me.simpleButton1.Name = "simpleButton1"
+            Me.simpleButton1.Size = New System.Drawing.Size(102, 23)
+            Me.simpleButton1.TabIndex = 1
+            Me.simpleButton1.Text = "ClearChildFilter"
+            ' 
+            ' panelControl1
+            ' 
+            Me.panelControl1.Controls.Add(Me.simpleButton4)
+            Me.panelControl1.Controls.Add(Me.simpleButton3)
+            Me.panelControl1.Controls.Add(Me.simpleButton2)
+            Me.panelControl1.Controls.Add(Me.simpleButton1)
+            Me.panelControl1.Dock = System.Windows.Forms.DockStyle.Top
+            Me.panelControl1.Location = New System.Drawing.Point(0, 0)
+            Me.panelControl1.Name = "panelControl1"
+            Me.panelControl1.Size = New System.Drawing.Size(790, 36)
+            Me.panelControl1.TabIndex = 2
+            ' 
+            ' simpleButton4
+            ' 
+            Me.simpleButton4.Location = New System.Drawing.Point(287, 5)
+            Me.simpleButton4.Name = "simpleButton4"
+            Me.simpleButton4.Size = New System.Drawing.Size(77, 23)
+            Me.simpleButton4.TabIndex = 4
+            Me.simpleButton4.Text = "BindingSource"
+            ' 
+            ' simpleButton3
+            ' 
+            Me.simpleButton3.Location = New System.Drawing.Point(204, 5)
+            Me.simpleButton3.Name = "simpleButton3"
+            Me.simpleButton3.Size = New System.Drawing.Size(77, 23)
+            Me.simpleButton3.TabIndex = 3
+            Me.simpleButton3.Text = "List"
+            ' 
+            ' simpleButton2
+            ' 
+            Me.simpleButton2.Location = New System.Drawing.Point(120, 5)
+            Me.simpleButton2.Name = "simpleButton2"
+            Me.simpleButton2.Size = New System.Drawing.Size(78, 23)
+            Me.simpleButton2.TabIndex = 2
+            Me.simpleButton2.Text = "DataTable"
+            ' 
+            ' Form1
+            ' 
+            Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
+            Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+            Me.ClientSize = New System.Drawing.Size(790, 481)
+            Me.Controls.Add(Me.gridControl1)
+            Me.Controls.Add(Me.panelControl1)
+            Me.Name = "Form1"
+            Me.Text = "Form1"
+            CType(Me.gridView2, System.ComponentModel.ISupportInitialize).EndInit()
+            CType(Me.gridControl1, System.ComponentModel.ISupportInitialize).EndInit()
+            CType(Me.gridView1, System.ComponentModel.ISupportInitialize).EndInit()
+            CType(Me.panelControl1, System.ComponentModel.ISupportInitialize).EndInit()
+            Me.panelControl1.ResumeLayout(False)
+            Me.ResumeLayout(False)
+
+        End Sub
+
+        #End Region
+
+        Private gridControl1 As DevExpress.XtraGrid.GridControl
+        Private gridView1 As DevExpress.XtraGrid.Views.Grid.GridView
+        Private WithEvents gridView2 As DevExpress.XtraGrid.Views.Grid.GridView
+        Private WithEvents simpleButton1 As DevExpress.XtraEditors.SimpleButton
+        Private panelControl1 As DevExpress.XtraEditors.PanelControl
+        Private WithEvents simpleButton4 As DevExpress.XtraEditors.SimpleButton
+        Private WithEvents simpleButton3 As DevExpress.XtraEditors.SimpleButton
+        Private WithEvents simpleButton2 As DevExpress.XtraEditors.SimpleButton
+    End Class
+End Namespace
+
